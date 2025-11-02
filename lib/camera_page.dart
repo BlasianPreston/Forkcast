d30973e74484b7daba7a82ca6dd70b6c30dd7ed8
Preston Williams <ipw5@cornell.edu>
2025-11-02 16:36:12 -0500

    fixing camera integration

--- lib/camera_page.dart
+++ lib/camera_page.dart
@@ -1,6 +1,35 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:camera_macos/camera_macos.dart';
+
+class PhotoPicker {
+  final ImagePicker _picker = ImagePicker();
+  late CameraMacOSController? _macController;
+
+  Future<String?> pickImage({required bool fromCamera}) async {
+    if (Platform.isMacOS) {
+      if (fromCamera) {
+        // Use macOS camera
+        CameraMacOSFile? file = await _macController?.takePicture();
+        if (file != null) {
+          return file.url;
+        }
+      } else {
+        // Use gallery picker on macOS
+        final XFile? file = await _picker.pickImage(
+          source: ImageSource.gallery,
+        );
+        return file?.path;
+      }
+    } else {
+      // iOS / Android
+      final source = fromCamera ? ImageSource.camera : ImageSource.gallery;
+      final XFile? file = await _picker.pickImage(source: source);
+      return file?.path;
+    }
+  }
+}
 
 class CameraPage extends StatefulWidget {
   const CameraPage({super.key});
